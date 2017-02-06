2f91c1c8984014d52d4f5d521a498ae515ce8748
Valentin <contact@eywek.fr>
2017-02-06 15:37:48 +0100

    add potentials double accounts

--- app/controller/UserController.js
+++ app/controller/UserController.js
@@ -1,4 +1,6 @@
 const api = new (require('../../vendors/eywek/obsifight/api'))(config.api.username, config.api.password)
+const async = require('async')
+const _ = require('underscore')
 
 module.exports = {
 
@@ -68,10 +70,102 @@ module.exports = {
       if (!result.status) // error
         return res.error(result.code, result.error)
 
-      // set vars
-      return res.render('user/get_user', {
-        user: result.body,
-        title: result.body.usernames.current
+      // find potential double account with IP + MAC
+      let user = result.body
+      user.accounts = []
+      /*{
+        username: 'bot4545',
+        lastConnection: '',
+        ip: '',
+        mac: ''
+      }*/
+      async.parallel([
+        // find with ip
+        (cb) => {
+          async.each(user.adresses.ip, (ip, callback) => {
+            api.request({
+              route: '/user/find',
+              method: 'post',
+              body: {
+                ip: ip
+              }
+            }, (err, result) => {
+              if (err) {
+                log.error('Error when login with API')
+                return
+              }
+              if (!result.status) // error
+                return
+              for (var i = 0; i < result.body.length; i++) {
+                user.accounts.push({
+                  username: result.body[i].username,
+                  lastConnection: result.body[i].last_connection,
+                  ip: ip,
+                  mac: undefined
+                })
+              }
+              callback()
+            })
+          }, () => {
+            cb()
+          })
+        },
+        // find with mac
+        (cb) => {
+          async.each(user.adresses.mac, (mac, callback) => {
+            api.request({
+              route: '/user/find',
+              method: 'post',
+              body: {
+                mac: mac
+              }
+            }, (err, result) => {
+              if (err) {
+                log.error('Error when login with API')
+                return
+              }
+              if (!result.status) // error
+                return
+              for (var i = 0; i < result.body.length; i++) {
+                user.accounts.push({
+                  username: result.body[i].username,
+                  lastConnection: result.body[i].last_connection,
+                  ip: undefined,
+                  mac: mac
+                })
+              }
+              callback()
+            })
+          }, () => {
+            cb()
+          })
+        }
+      ], (err, results) => {
+        // formatting accounts
+        let accounts = {}
+        for (var i = 0; i < user.accounts.length; i++) {
+          if (user.accounts[i].username === user.usernames.current || _.find(user.usernames.histories, function(obj) { return obj.username == user.accounts[i].username })) // current user
+            continue
+          if (accounts[user.accounts[i].username] === undefined) { // add to accounts
+            accounts[user.accounts[i].username] = {
+              lastConnection: user.accounts[i].lastConnection,
+              ip: (user.accounts[i].ip) ? [user.accounts[i].ip] : [],
+              mac: (user.accounts[i].mac) ? [user.accounts[i].mac] : []
+            }
+          } else { // add IP/Mac, edit lastConnection
+            accounts[user.accounts[i].username].lastConnection = user.accounts[i].lastConnection
+            if (user.accounts[i].ip)
+              accounts[user.accounts[i].username].ip.push(user.accounts[i].ip)
+            if (user.accounts[i].mac)
+              accounts[user.accounts[i].username].mac.push(user.accounts[i].mac)
+          }
+        }
+        user.accounts = accounts
+        // set vars
+        return res.render('user/get_user', {
+          user: user,
+          title: user.usernames.current
+        })
       })
     })
   },
